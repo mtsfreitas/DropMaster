94260d32abd6acda029ffb6b164ebd9f9be540d3
Matheus <mtsftsmts@ufv.br>
2023-06-24 22:52:43 -0300

    changing html folder

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 
 # Carregando o modelo e o LabelEncoder
 with open('model.pkl', 'rb') as f:
